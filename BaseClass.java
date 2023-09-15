81c2e1238f22506d232807598b1ffb7d33daf2b7
Potevedika <145018589+Potevedika@users.noreply.github.com>
2023-09-15 18:37:19 +0530

    Add files via upload

--- BaseClass.java
+++ BaseClass.java
@@ -1,18 +1,20 @@
-package schoolwebapp;
+package libraryFiles;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseClass 
 {
-WebDriver driver;
-	
+	WebDriver driver;
 	public void initializeBrowser() throws IOException 
 	{
-		System.setProperty("webdriver.chrome.driver", 
-				"C:\\Users\\hp\\Downloads\\chromedriver_win32");                    
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chrome-win32");                    
 	     driver=new ChromeDriver();
 		driver.get(UtilityClass.readDataFromPF("URL"));
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));	
 	}
-
-}
 }
--- UtilityClass.java
+++ UtilityClass.java
@@ -1,25 +1,14 @@
-package schoolwebapp;
+package libraryFiles;
 
-import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
-public class UtilityClass 
+public class UtilityClass
 {
-	public static String getTD(int rowIndex,int colIndex) throws EncryptedDocumentException, IOException
-	{
-		FileInputStream file=new FileInputStream("C:\\Users\\sanja\\OneDrive\\Desktop\\Study\\3rd Sept Mrng\\3rdSeptMrngA.xlsx");
-		 Sheet sh = WorkbookFactory.create(file).getSheet("DDF");
-	
-		String value = sh.getRow(rowIndex).getCell(colIndex).getStringCellValue();	
-		return value;
-	}
-	
-	
-	
 	public static String readDataFromPF(String key) throws IOException 
 	{
-		FileInputStream file=new FileInputStream("C:\\Users\\hp\\eclipse-workspace\\schoolwebapp\\PropertyFile.properties");
+		FileInputStream file=new FileInputStream("C:\\Users\\hp\\eclipse-workspace\\Sample\\propertyFile.properties");
 		
 		Properties p=new Properties();
 		p.load(file);
@@ -27,7 +16,4 @@ public class UtilityClass
 		String value = p.getProperty(key);
 		return value;
 	}
-	
-
 }
-
